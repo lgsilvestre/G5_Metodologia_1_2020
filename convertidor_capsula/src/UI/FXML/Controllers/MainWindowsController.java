1c88bdce38f720255d3fed00bbdca8d8e9d7cb45
okaminos <sanfurgoh@gmail.com>
2020-07-03 13:42:44 -0400

    MainWindowsController
    Empty controller to mainwindowscontroller

---
+++ convertidor_capsula/src/UI/FXML/Controllers/MainWindowsController.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI.FXML.Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author isanfurg
+ */
+public class MainWindowsController implements Initializable {
+
+    @FXML
+    private Button helpButton;
+    @FXML
+    private TextField wordsField;
+    @FXML
+    private TextField xField;
+    @FXML
+    private TextField yField;
+    @FXML
+    private TextField rotationField;
+    @FXML
+    private TextField exprField;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button buttonInvert;
+    @FXML
+    private Button buttonPoints;
+    @FXML
+    private Button buttonApply;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void help(ActionEvent event) {
+    }
+
+    @FXML
+    private void wordsTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void XTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void yTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void rotationTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void exprTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void buttonOnePressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonTwoPressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonThreePressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonFourPressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonFivePressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonSixPressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void buttonSevenPressed(ActionEvent event) {
+    }
+
+    @FXML
+    private void invert(ActionEvent event) {
+    }
+
+    @FXML
+    private void showPoints(ActionEvent event) {
+    }
+
+    @FXML
+    private void apply(ActionEvent event) {
+    }
+    
+}
